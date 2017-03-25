ddef45e1415ea8979c69055e2f7f406350a2ab6f
kermage <genealyson.torcende@gmail.com>
2017-03-25 23:15:25 +0800

    Initial commit; user meta box

---
+++ includes/class-themeplate-usermeta.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Setup user meta boxes
+ *
+ * @package ThemePlate
+ * @since 0.1.0
+ */
+
+
+class ThemePlate_UserMeta {
+
+	private $meta_box;
+
+
+	public static function instance() {
+
+		return new self();
+
+	}
+
+
+	public function __construct() {
+
+
+	}
+
+
+	public function add( $meta_box ) {
+
+
+	}
+
+
+	public function create( $user ) {
+
+
+	}
+
+
+	public function save( $user_id ) {
+
+
+	}
+
+}
