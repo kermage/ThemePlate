eb47af22ed38656eb50ba59ebba0afa9b4ea3e08
genepbwebdev <gene@pbwebdev.com.au>
2017-08-03 16:00:19 +0800

    fix breaking option; unwanted line removal from prev commit

--- includes/class-themeplate-settings.php
+++ includes/class-themeplate-settings.php
@@ -209,6 +209,7 @@ class ThemePlate_Settings {
 			}
 
 			$field['id'] = $param['id'] . '_' . $id;
+			$field['page'] = ( $param['page'] ? $param['page'] : 'options' );
 			$label = $field['name'] . ( $field['desc'] ? '<span>' . $field['desc'] . '</span>' : '' );
 
 			add_settings_field(
