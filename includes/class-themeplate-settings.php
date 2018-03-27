f0477f8f01473a52f3a56bfa4b1325a8095112e0
kermage <genealyson.torcende@gmail.com>
2018-03-27 19:47:14 +0800

    action a little later so we can check here too if we're on the correct screen

--- includes/class-themeplate-settings.php
+++ includes/class-themeplate-settings.php
@@ -22,7 +22,7 @@ class ThemePlate_Settings {
 			return false;
 		}
 
-		add_action( 'admin_init', array( $this, 'create' ) );
+		add_action( 'current_screen', array( $this, 'create' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'scripts_styles' ) );
 
 	}
@@ -32,9 +32,13 @@ class ThemePlate_Settings {
 
 		$settings = $this->tpmb->get_config();
 		$key = ThemePlate()->key . '-' . ( isset( $settings['page'] ) ? $settings['page'] : ThemePlate()->slug );
-		$page = $key . '-' . ( isset( $settings['context'] ) ? $settings['context'] : 'normal' );
-
 		$this->key = $key;
+
+		if ( ! $this->is_valid_screen() ) {
+			return;
+		}
+
+		$page = $key . '-' . ( isset( $settings['context'] ) ? $settings['context'] : 'normal' );
 
 		add_action( 'themeplate_settings_' . $page, array( $this, 'add' ) );
 
@@ -50,13 +54,24 @@ class ThemePlate_Settings {
 
 	public function scripts_styles() {
 
-		$screen = get_current_screen();
-
-		if ( strpos( $screen->id, $this->key ) === false ) {
+		if ( ! $this->is_valid_screen() ) {
 			return;
 		}
 
 		$this->tpmb->enqueue();
+
+	}
+
+
+	private function is_valid_screen() {
+
+		$screen = get_current_screen();
+
+		if ( strpos( $screen->id, $this->key ) === false ) {
+			return false;
+		}
+
+		return true;
 
 	}
 
