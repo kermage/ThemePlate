3bdff559c126f60bfeff0f13b82c01e76d8633a6
kermage <genealyson.torcende@gmail.com>
2018-03-31 01:08:27 +0800

    missed; transpose space and quote

--- includes/class-themeplate-cleaner.php
+++ includes/class-themeplate-cleaner.php
@@ -48,8 +48,8 @@ class ThemePlate_Cleaner {
 		remove_action( 'embed_head', 'print_emoji_detection_script', 7 );
 		remove_action( 'admin_print_styles', 'print_emoji_styles' );
 		remove_action( 'wp_print_styles', 'print_emoji_styles' );
-		remove_filter( 'the_content_feed', 'wp_staticize_emoji ');
-		remove_filter( 'comment_text_rss', 'wp_staticize_emoji ');
+		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 
 		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
