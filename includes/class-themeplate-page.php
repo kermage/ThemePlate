43fa9fdda1f0edbeebd0dd8f1a0639ca0b5414d5
kermage <genealyson.torcende@gmail.com>
2018-03-31 12:02:31 +0800

    this is using a literal string here

--- includes/class-themeplate-page.php
+++ includes/class-themeplate-page.php
@@ -113,7 +113,7 @@ class ThemePlate_Page {
 			return;
 		}
 
-		if ( $_REQUEST['page'] === $this->config['id'] && $_REQUEST['settings-updated'] === true ) {
+		if ( $_REQUEST['page'] === $this->config['id'] && $_REQUEST['settings-updated'] === 'true' ) {
 			echo '<div id="themeplate-message" class="updated"><p><strong>Settings updated.</strong></p></div>';
 		}
 
