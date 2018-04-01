11cd70da96a9af6b18fc8e64811811fa1de3ff00
kermage <genealyson.torcende@gmail.com>
2018-04-01 18:07:37 +0800

    type-cast as array; we could only have 1 field

--- includes/class-themeplate-helpers.php
+++ includes/class-themeplate-helpers.php
@@ -139,7 +139,7 @@ class ThemePlate_Helpers {
 
 		$filtered = array();
 
-		foreach ( $data as $key => $value ) {
+		foreach ( (array) $data as $key => $value ) {
 			if ( is_array( $value ) ) {
 				$value = self::filter( $value );
 			}
