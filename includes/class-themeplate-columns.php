5528a26c36037ce4a15dd3dab62b23ad25baf4c8
kermage <genealyson.torcende@gmail.com>
2018-04-10 23:40:41 +0800

    add the basic methods to add and display a column

--- includes/class-themeplate-columns.php
+++ includes/class-themeplate-columns.php
@@ -19,6 +19,39 @@ class ThemePlate_Columns {
 			throw new Exception();
 		}
 
+		if ( ! array_key_exists( 'post_type', $config ) || ! array_key_exists( 'id', $config ) || ! array_key_exists( 'title', $config ) || ! array_key_exists( 'callback', $config ) ) {
+			throw new Exception();
+		}
+
+		$this->config = $config;
+
+		add_filter( 'manage_' . $config['post_type'] . '_posts_columns', array( $this, 'modify' ), 10 );
+		add_action( 'manage_' . $config['post_type'] . '_posts_custom_column', array( $this, 'populate' ), 10, 2 );
+
+	}
+
+
+	public function modify( $columns ) {
+
+		$config = $this->config;
+
+		$columns[$config['id']] = $config['title'];
+
+		return $columns;
+
+	}
+
+
+	public function populate( $column_name, $post_id ) {
+
+		$config = $this->config;
+
+		if ( $column_name !== $config['id'] ) {
+			return;
+		}
+
+		return call_user_func( $config['callback'], $post_id );
+
 	}
 
 }
