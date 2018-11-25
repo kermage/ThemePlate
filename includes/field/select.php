1d15b7834ee3659e0b881c7aa746e0ab536657ee
kermage <genealyson.torcende@gmail.com>
2018-11-25 21:21:19 +0800

    need no value for form validation(required) to work

--- includes/field/select.php
+++ includes/field/select.php
@@ -18,7 +18,7 @@ class ThemePlate_Field_Select {
 		if ( 'select2' === $field['type'] && ! $field['value'] ) {
 			echo '<option></options>';
 		} elseif ( 'select2' !== $field['type'] && ( ( $field['none'] && $field['value'] ) || ( ! $field['multiple'] && ! $field['value'] ) ) ) {
-			echo '<option value="0"' . ( $field['none'] && $field['value'] ? '' : ' disabled hidden' ) . ( $field['value'] ? '>' . __( '&mdash; None &mdash;' ) : ' selected>' . __( '&mdash; Select &mdash;' ) ) . '</option>';
+			echo '<option value=""' . ( $field['none'] && $field['value'] ? '' : ' disabled hidden' ) . ( $field['value'] ? '>' . __( '&mdash; None &mdash;' ) : ' selected>' . __( '&mdash; Select &mdash;' ) ) . '</option>';
 		}
 		if ( 'select2' === $field['type'] && $field['multiple'] && $field['value'] ) {
 			$ordered = array();
