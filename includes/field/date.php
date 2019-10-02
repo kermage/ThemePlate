218c4c96b796c581a2de5d4f15453d655e501d3b
kermage <genealyson.torcende@gmail.com>
2019-10-02 08:17:20 +0800

    allow custom datepicker options via data-attributes

--- includes/field/date.php
+++ includes/field/date.php
@@ -16,11 +16,23 @@ class ThemePlate_Field_Date {
 
 		if ( $field['multiple'] ) {
 			echo '<div id="' . esc_attr( $field['id'] ) . '" class="themeplate-date-picker multiple">';
-			echo '<input type="hidden" name="' . esc_attr( $field['name'] ) . '" value="' . esc_attr( $field['value'] ) . '"' . ( $field['none'] ? ' data-none="true"' : '' ) . ' />';
+			echo '<input type="hidden" name="' . esc_attr( $field['name'] ) . '" value="' . esc_attr( $field['value'] ) . '"' . ( $field['none'] ? ' data-none="true"' : '' );
+			if ( ! empty( $field['options'] ) ) {
+				foreach ( $field['options'] as $option => $value ) {
+					echo 'data-date-' . esc_attr( $option ) . '="' . esc_attr( $value ) . '"';
+				}
+			}
+			echo ' />';
 			echo '</div>';
 			echo '<ul class="ul-disc"></ul>';
 		} else {
-			echo '<input type="text" name="' . esc_attr( $field['name'] ) . '" id="' . esc_attr( $field['id'] ) . '" class="themeplate-date-picker single" value="' . esc_attr( $field['value'] ) . '"' . ( $field['none'] ? ' data-none="true"' : '' ) . ' />';
+			echo '<input type="text" name="' . esc_attr( $field['name'] ) . '" id="' . esc_attr( $field['id'] ) . '" class="themeplate-date-picker single" value="' . esc_attr( $field['value'] ) . '"' . ( $field['none'] ? ' data-none="true"' : '' );
+			if ( ! empty( $field['options'] ) ) {
+				foreach ( $field['options'] as $option => $value ) {
+					echo 'data-date-' . esc_attr( $option ) . '="' . esc_attr( $value ) . '"';
+				}
+			}
+			echo ' />';
 		}
 
 		echo '</div>';
