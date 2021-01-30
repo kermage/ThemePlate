6d5bb39a5d384302a3c735f262228b2f0ea14e35
kermage <genealyson.torcende@gmail.com>
2021-01-30 21:29:58 +0800

    just need to do the cleaning via filters now

--- includes/NavWalker.php
+++ includes/NavWalker.php
@@ -9,15 +9,16 @@
 
 namespace ThemePlate;
 
-class NavWalker extends \Walker {
+class NavWalker extends \Walker_Nav_Menu {
 
-	public $tree_type = array( 'post_type', 'taxonomy', 'custom' );
+	public function __construct() {
 
+		add_filter( 'nav_menu_submenu_css_class', array( $this, 'submenu_css_class' ), 10 );
+		add_filter( 'nav_menu_css_class', array( $this, 'css_class' ), 10, 4 );
+		add_filter( 'nav_menu_item_id', array( $this, 'item_id' ), 10, 2 );
+		add_filter( 'nav_menu_link_attributes', array( $this, 'link_attributes' ), 10, 3 );
 
-	public $db_fields = array(
-		'parent' => 'menu_item_parent',
-		'id'     => 'db_id',
-	);
+	}
 
 
 	public $class = array(
@@ -34,31 +35,17 @@ class NavWalker extends \Walker {
 	}
 
 
-	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+	public function submenu_css_class() {
 
-		$classes = array( $this->class['sub-menu'] );
-
-		$class_names = implode( ' ', apply_filters( 'nav_menu_submenu_css_class', $classes, $args, $depth ) );
-		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
-
-		$output .= '<ul' . $class_names . '>';
+		return array( $this->class['sub-menu' ] );
 
 	}
 
 
-	public function end_lvl( &$output, $depth = 0, $args = array() ) {
+	public function css_class( $classes, $item, $args ) {
 
-		$output .= '</ul>';
+		$classes = array();
 
-	}
-
-
-	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-
-		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-		$classes = preg_replace( '/current[-_](menu|page)[-_](item|parent|ancestor)|(menu|page)[-_\w+]+/', '', $classes );
-
-		/** @var stdClass $args */
 		if ( $args->walker->has_children ) {
 			$classes[] = $this->class['has-sub'];
 		}
@@ -67,61 +54,27 @@ class NavWalker extends \Walker {
 			$classes[] = $this->class['active'];
 		}
 
-		$args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
-
-		$class_names = implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
-		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
-
-		$id_name = apply_filters( 'nav_menu_item_id', '', $item, $args, $depth );
-		$id_name = $id_name ? ' id="' . esc_attr( $id_name ) . '"' : '';
-
-		$output .= '<li' . $id_name . $class_names . '>';
-
-		$atts = array();
-		// phpcs:disable WordPress.WhiteSpace.OperatorSpacing.SpacingBefore
-		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
-		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
-		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
-		// phpcs:enable
-
-		if ( '_blank' === $item->target && empty( $item->xfn ) ) {
-			$atts['rel'] = 'noopener noreferrer';
-		} else {
-			$atts['rel'] = $item->xfn;
-		}
-
-		$atts['aria-current'] = $item->current ? 'page' : '';
-
-		$atts = array_merge( $atts, $this->attributes( $item, $args ) );
-		$atts = array_filter( $atts );
-		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
-
-		$attributes = '';
-		foreach ( $atts as $attr => $value ) {
-			if ( ! empty( $value ) ) {
-				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
-
-				$attributes .= ' ' . $attr . '="' . $value . '"';
-			}
-		}
-
-		$title = apply_filters( 'the_title', $item->title, $item->ID );
-		$title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
-
-		$item_output  = $args->before;
-		$item_output .= '<a' . $attributes . '>';
-		$item_output .= $args->link_before . $title . $args->link_after;
-		$item_output .= '</a>';
-		$item_output .= $args->after;
-
-		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+		return array_unique( $classes );
 
 	}
 
 
-	public function end_el( &$output, $item, $depth = 0, $args = array() ) {
+	public function item_id( $id, $item ) {
 
-		$output .= '</li>';
+		if ( 'menu-item-' . $item->ID === $id ) {
+			$id = '';
+		}
+
+		return $id;
+
+	}
+
+
+	public function link_attributes( $atts, $item, $args ) {
+
+		$atts = array_merge( $atts, $this->attributes( $item, $args ) );
+
+		return array_filter( $atts );
 
 	}
 
