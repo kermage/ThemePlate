b75ca84464973b557bbcc2b8f5e11e93d687b00d
kermage <genealyson.torcende@gmail.com>
2021-11-06 05:18:20 +0800

    loop through all available features

--- class-themeplate.php
+++ class-themeplate.php
@@ -183,14 +183,9 @@ class ThemePlate {
 
 	public function cleanup() {
 
-		add_theme_support( 'tpc_wp_head' );
-		add_theme_support( 'tpc_emoji_detection' );
-		add_theme_support( 'tpc_query_strings' );
-		add_theme_support( 'tpc_dependency_tag' );
-		add_theme_support( 'tpc_unnecessary_class' );
-		add_theme_support( 'tpc_extra_styles' );
-		add_theme_support( 'tpc_embed_wrap' );
-		add_theme_support( 'tpc_nav_walker' );
+		foreach ( array_keys( Cleaner::features() ) as $feature ) {
+			add_theme_support( $feature );
+		}
 
 	}
 
